17ba701743eb87d1bfa4be79d94901f288961c4c
Sean Bigelow <sean.bigelow1@gmail.com>
2020-11-08 19:52:40 +0000

    Done.

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,16 +1,31 @@
-def roll_call_dwarves# code an argument here
-  # Your code here
+require 'pry'
+
+
+def roll_call_dwarves(roll)
+  roll.each_with_index do |key, index|
+    puts "#{index + 1}. #{key}"
+ end
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(plan)
+ array = []
+ 
+ plan.each do |key| 
+   array << "#{key.capitalize}!"
+ 
+ end
+ array
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(long)
+  
+  long.any? {|key| key.length > 4}
+ 
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(cheese)
+
   cheese_types = ["cheddar", "gouda", "camembert"]
+  cheese.find {|key| key == "cheddar"}
+  
 end
